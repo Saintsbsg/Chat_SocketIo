c4e31beb910f36dc82ca738400b6a89a7101633c
Saintsbsg <gabrielsantos_bsg@outlook.com>
2022-04-03 09:21:09 -0300

    Inícío do Projeto

---
+++ index.js
@@ -0,0 +1,23 @@
+let express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+let io = require("socket.io")(http);
+app.set('view engine', 'ejs');
+
+
+io.on("connection",(socket) =>{
+    socket.on("boasvindas", (data) =>{
+        console.log("Executando o evento de boas vindas")
+        console.log(data);
+    });
+})
+
+app.get("/", (req,res) =>{
+    res.render("index")
+});
+
+
+
+http.listen(8181, ()=>{
+    console.log("rodando!");
+})
